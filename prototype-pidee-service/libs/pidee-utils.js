5f38a5809a7129c998b45342bf61600c4c75e37f
Tommaso Lanza <t@theworkers.net>
2015-06-29 01:02:47 +0100

    Utils: get UID from username
    Using
    $ id -u [username]

--- prototype-pidee-service/libs/pidee-utils.js
+++ prototype-pidee-service/libs/pidee-utils.js
@@ -4,6 +4,7 @@
 // Modules
 // =======
 var W = require( 'w-js' );
+var exec = require('child_process').exec;
 
 // Privilages
 // ==========
@@ -17,6 +18,22 @@ function dropSuperUserPrivilages () {
         throw new Error( 'Unable to get non-sudo user' );
     }
     process.setuid( process.env.SUDO_USER );
+}
+
+function getUIDFromUsername ( username, callback ) {
+    if (typeof username === 'function') {
+        callback = username;
+        username = undefined;
+    }
+    username = username || 'pidee';
+    callback = callback || function () {};
+
+    var child = exec('id -u ' + username, {uid: 501}, function ( err, stdout, stderr ) {
+        if ( err ) {
+            return callback( err );
+        }
+        callback( null, stdout );
+    });
 }
 
 // Reporting
@@ -38,6 +55,19 @@ function makeReporter( status, str ) {
 
 // Promisers
 // ---------
+
+function getUIDFromUsernamePromise ( username ) {
+    username = username || 'pidee';
+    var args = arguments;
+    return W.promise( function ( resolve, reject ) {
+        getUIDFromUsername( username, function (err, stdout) {
+            if ( err ) {
+                return reject( err );
+            }
+            resolve.apply( this, args );
+        });
+    });
+}
 
 function confirmSuperUserPromise () {
     var args = arguments;
@@ -70,6 +100,7 @@ module.exports = {
     isSuperUser: isSuperUser,
     dropSuperUserPrivilages: dropSuperUserPrivilages,
     confirmSuperUserPromise: confirmSuperUserPromise,
+    getUIDFromUsernamePromise: getUIDFromUsernamePromise,
     makeReporter: makeReporter,
     report: report,
     promiseWrap: promiseWrap
